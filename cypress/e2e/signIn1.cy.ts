e18780b5463d9c597294c6e7ad318691b0dd1898
Artem Eroshenko <artem.eroshenko@qameta.io>
2024-07-22 12:53:22 +0300

    add allure integration (via #1)

--- cypress/e2e/signIn1.cy.ts
+++ cypress/e2e/signIn1.cy.ts
@@ -16,3 +16,20 @@ it("sample test", () => {
     });
   });
 })
+
+it("one more test", () => {
+  label("foo", "bar");
+  link("https://allurereport.org", "custom", "Allure Web-Site");
+  issue("https://allurereport.org/fake-issue/1", "Issue 1");
+  issue("2", "Issue 2");
+  tms("https://allurereport.org/fake-task/1", "Task 1");
+  tms("2", "Task 2");
+  attachment("Sample text attachment", "Hello world!", "text/plain");
+  step("step 1", () => {
+    step("step 1.1", () => {
+      step("step 1.1.1", () => {
+        attachment("Sample text attachment for step", "Goodbye world!", "text/plain");
+      });
+    });
+  });
+})
